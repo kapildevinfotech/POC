8a866586582c67749ca8e0c4ad120f3649cd8ff9
kapildevinfotech <kapildevinfotech@gmail.com>
2020-12-16 20:57:51 +0530

    Recursive Binary search

--- src/main/java/com/sapient/ds/search/BinarySearch.java
+++ src/main/java/com/sapient/ds/search/BinarySearch.java
@@ -3,8 +3,9 @@ package com.sapient.ds.search;
 public class BinarySearch {
     public static void main(String[] args) {
         int[] a={2,3,5,6,7,8,9};
-        int x=3;
-        int p=binarySearch(a,x);
+        int x=8;
+        //int p=binarySearch(a,x);
+        int p=recursiveBinarySearch(a,x,0,a.length-1);
         System.out.printf("Element %d found at %d%n",x,p);
     }
 
@@ -25,5 +26,19 @@ public class BinarySearch {
             }
         }
         return -1;
+    }
+
+    private static int recursiveBinarySearch(int[] a, int x,int left,int right) {
+        if (left > right) {
+            return -1;
+        }
+        int mid = (left + right) / 2;
+        if (x == a[mid]) {
+            return mid;
+        } else if (x > a[mid]) {
+           return recursiveBinarySearch(a, x, mid + 1, right);
+        } else {
+            return recursiveBinarySearch(a, x, left, mid - 1);
+        }
     }
 }
